c3d98212a5dbb1499b0320c4f183b64752df9d02
Eiichi <work.eiichi.co.jp@gmail.com>
2019-06-17 16:48:32 +0000

    初期ファイル追加

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # tampermonkey-gitlab-mr-page-alert
+
+## これは何？
+
+
+## 導入
+
+### 1. ユーティリティ画面を開きます
+
+[ユーティリティ](chrome-extension://dhdgffkkebhmkfjojejmpbldmpobfkfo/options.html#nav=utils)
+
+### URLに入力します
+
+
+###
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         tampermonkey-gitlab-mr-page-alert
+// @namespace    https://github.com/eiichi-worker/tampermonkey-gitlab-mr-page-alert
+// @version      2019-06-18
+// @description  GitLabのマージリクエストページを安全に！
+// @author       Eiichi Fujimoto
+// @match
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
