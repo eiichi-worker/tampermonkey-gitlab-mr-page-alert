26de4aadf160816898d2949468db888f27bac927
eiichi-worker <8284623+eiichi-worker@users.noreply.github.com>
2019-06-19 00:35:22 +0000

    機能実装

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@
 // @version      2019-06-18
 // @description  GitLabのマージリクエストページを安全に！
 // @author       Eiichi Fujimoto
-// @match
+// @match        https://gitlab.com/*/merge_requests/*
 // @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+    const ALERT_BRANCH = "master"
 
-    // Your code here...
+    let targetBranch = $(".js-target-branch").text().trim()
+    let mergeButton = $("button.qa-merge-button")
+
+    if (targetBranch == ALERT_BRANCH) {
+        // Button背景変更
+        mergeButton.css('background-color', 'Red').css('color', 'Yellow')
+        // ボックスの背景変更
+        $(".mr-state-widget div").css('background-color', 'lavenderblush')
+        // 注意メッセージ表示
+        $(".mr-widget-body-controls").append("<b>【注意】" + ALERT_BRANCH + "ブランチへのマージリクエストです!</b>")
+    }
 })();
